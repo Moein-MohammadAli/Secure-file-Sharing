7d3eb1db5dac8447eaa75ac4047897b753b0ffcd
Mohammad Ali <mrmapaia.3@gmail.com>
2020-07-30 12:41:32 +0430

    fixing Cryptography bugs.

--- core/utils/CryptographyModule.py
+++ core/utils/CryptographyModule.py
@@ -8,12 +8,13 @@ from rest_framework.exceptions import ValidationError
 import struct
 import json
 
-<<<<<<< HEAD
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+# from django.conf import settings
+# from backend.settings import P, PUBLIC_KEY, PRIVATE_KEY
+
+
 sys.path.append('../../backend')
 from settings import P, PUBLIC_KEY, PRIVATE_KEY
-=======
-from backend.settings import P, G, PUBLIC_KEY, PRIVATE_KEY
->>>>>>> 816bff5f1be0b72b1385ff21ac90a17d30276c15
 
 class CryptoCipher(object):
 
@@ -75,48 +76,11 @@ class CryptoCipher(object):
             fo.write(dec)
 
     def private_key_decryption(self, private_key, encrypted):
-        return str(pow(encrypted, int(PRIVATE_KEY, 16), int(self.p, 16)))
+        return str(pow(int(encrypted), int(private_key, 16), int(self.p, 16)))
 
     def public_key_encryption(self, public_key, plain_text):
         return str(pow(int(plain_text, 16), int(public_key, 16), int(self.p, 16)))
 
-
-
-
-# some example to show how to use this module
-if __name__=="__main__":
-    ## Using to encrypt/decrypt a 'string' message with given key
-    
-
-    # session_key = pow(int(PUBLIC_KEY_client, 16), int(PRIVATE_KEY, 16), int(P, 16))
-    # print(session_key)
-
-    # ses = 12102292340787506527703514819128586984757329604944520300483314667474312146401387724578562014938173459611215076980505992852635232655068555405060650525566178029715405285302655764946200132300458136951341989263187824994655485923896641660706255421345970417306891397652712803859750564638556741855530638263193012867978806300613860896538482501056953232760257703174859232871452549767671649657487467801173885382121024863158317837902897735687508938742799694028848280123028608192431484369926324068390881839758415967377098367720623409825036375949914340426574884923240519705637188437856002015567603773793884911654806404721057780096
-    # pub = session_key
-    
-    
-    # a = pow(10, PUBLIC_KEY, int(P,16))
-    a = 19932357042291180844281721172973927269566136734323207371678966231752835204083982805860019791127650045939347819243766056711379400501942059898360220047179625449366095347511213770686915251832194026883299160780231110667053777554982902270114288632671912173533514562297893854700560210229930982968478569241663740469109484114886374414824896006361919401304592874563567018721715033957026147167104904516719861063354608168253066913186257285443504965057920154406683006324471503139064927667818305537471683175176648683853124143758477153047801051914739172841448203423207942053136818162769295227948176016567597915337290889925202789574
-    # b = pow(a, int(PRIVATE_KEY, 16), int(P,16))
-    # print(b)
-
-    cipher = CryptoCipher(a)
-    a = "{}".format({"username":"moein","password":"12345","confidentiality_label": 1,"integrity_label": 1})
-    print(a)
-    c = cipher.encrypt_text(a)
-    print(c)
-    e = b'cE8EYTWGqyapOnenaW70eHQ7rMEasblG4r+ttKxfpOElzt0+SNbqNK7IG+weCCK1NWK9te1d3lxjFrBvHAmPDePgU5JtUxZcirB5E29I+9gMaHXlCwYYRteUcT0/vUmbo+MY5G7bbtVok+sZjiIY5w=='
-    print(cipher.decrypt_text(e))
-
-    ### Using to encrypt/decrypt any file with specified key
-
-    # a.encrypt_file("./scr.png")
-    # a.decrypt_file("scr.png.enc")
-
-<<<<<<< HEAD
-    # a.encrypt_file("data.txt")
-    # a.decrypt_file("data.txt.enc")
-=======
 
 def get_data(request):
     headers = request.headers
@@ -125,4 +89,3 @@ def get_data(request):
     else:
         raise ValidationError("session_key is invalid.")
 
->>>>>>> 816bff5f1be0b72b1385ff21ac90a17d30276c15
